43334fced97dbfac2ded0ee0faf7254f045aee47
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2025-04-26 19:58:32 -0300

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,38 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
 import { vMaska } from "maska/vue"
+import { ref, watch } from 'vue'
+
+const cep = ref("");
+const uf = ref("");
+const cidade = ref("");
+const rua = ref("");
+const bairro = ref("");
+
+watch([cep], async ([novoCep], []) => {
+	if (novoCep.length !== 9) {
+		return;
+	}
+
+	novoCep = novoCep.replace("-", "");
+
+	try {
+		const resp = await fetch(`https://viacep.com.br/ws/${novoCep}/json/`);
+		const data = await resp.json();
+
+		if (data.erro) {
+			alert('CEP não encontrado');
+			return;
+		}
+
+		// Preenche campos
+		uf.value = data.uf;
+		cidade.value = data.localidade;
+		rua.value = data.logradouro;
+		bairro.value = data.bairro;
+	} catch (error) {
+		console.error('Erro na consulta CEP:', error);
+	}
+});
 </script>
 
 <template>
@@ -10,80 +42,74 @@ import { vMaska } from "maska/vue"
 		</header>
 		<main
 			class="flex flex-col justify-center w-5/6 lg:w-3/4 xl:w-3/5 text-base bg-white border-2 border-gray-400 p-4 rounded-lg">
-			<div class="flex flex-col md:flex-row gap-2 py-2 md:items-center">
-				<div class="w-full md:w-50">Nome completo:</div>
-				<input type="text" class="border-2 border-gray-400 rounded-lg p-1 w-full" />
-			</div>
 
-			<div class="flex justify-between flex-col lg:flex-row lg:gap-6">
-				<div class="flex flex-col md:flex-row gap-2 py-2 md:items-center">
-					<div class="w-50">Data de nascimento:</div>
-					<input type="date" class="border-2 border-gray-400 rounded-lg p-1" />
+			<div class="flex flex-col justify-between md:flex-row md:gap-6">
+				<div class="flex flex-col gap-2 pb-2 w-full">
+					<div class="w-full md:w-38">Nome completo</div>
+					<input type="text" class="border-2 border-gray-400 rounded-lg p-1 w-full" />
 				</div>
-
-				<div class="flex flex-col md:flex-row gap-2 py-2 md:items-center w-full">
-					<div class="w-50">CPF ou CNPJ</div>
-					<input v-maska="'###.###.###-##'" type="text" class="border-2 border-gray-400 rounded-lg p-1 w-full" />
+				<div class="flex flex-col gap-2 py-2 md:pt-0">
+					<div class="w-38">Data de nascimento</div>
+					<input type="date" class="border-2 border-gray-400 rounded-lg p-1 w-36" />
 				</div>
 			</div>
+			<div class="flex flex-col md:flex-row md:gap-6">
+				<div class="flex flex-col gap-2 py-2">
+					<div class="w-38">Telefone</div>
+					<input type="text" v-maska="'(##) #####-####'"
+						class="border-2 border-gray-400 rounded-lg p-1 w-38" />
+				</div>
 
-			<!-- <div>
-				<div>Endereço completo</div>
-				<div class="flex flex-col p-6">
-					<div class="flex justify-between flex-col md:flex-row flex-1">
-						<div class="flex gap-2 py-4">
-							<div>Rua</div>
-							<input type="text" class="bg-gray-200 flex-1">
-						</div>
-						<div class="flex gap-2 py-4">
-							<div>Bairro</div>
-							<input type="text" class="bg-gray-200 flex-1">
-						</div>
-					</div>
-					<div class="flex justify-between">
-						<div class="flex gap-2 py-4">
-							<div>Número</div>
-							<input type="text" class="bg-gray-200">
-						</div>
-						<div class="flex gap-2 py-4">
-							<div>Cidade</div>
-							<input type="text" class="bg-gray-200">
+				<div class="flex flex-col gap-2 py-2">
+					<div class="w-10">CPF</div>
+					<input v-maska="'###.###.###-##'" type="text"
+						class="border-2 border-gray-400 rounded-lg p-1 w-36" />
+				</div>
 
-						</div>
-
-					</div>
-					<div class="flex justify-between">
-						<div class="flex gap-2 py-4">
-							<div>Estado</div>
-							<input type="text" class="bg-gray-200">
-						</div>
-						<div class="flex gap-2 py-4">
-							<div>CEP</div>
-							<input type="text" class="bg-gray-200">
-						</div>
-					</div>
+				<div class="flex flex-col gap-2 py-2 w-full">
+					<div class="w-14">E-mail</div>
+					<input type="text" class="border-2 border-gray-400 rounded-lg p-1 w-full" />
 				</div>
 			</div>
 
-			<div class="flex gap-2 py-4">
-				<div>telefone</div>
-				<input type="text" class="bg-gray-200">
-			</div>
+			<div class="">
+				<div class="py-2">Endereço completo</div>
 
-			<div class="flex gap-2 py-4">
-				<div>email</div>
-				<input type="text" class="bg-gray-200">
-			</div>
+				<div class="flex flex-col md:flex-row md:gap-6 w-full">
 
-			<div class="flex gap-2 py-4">
-				<div>data do preenchimento</div>
-				<input type="text" class="bg-gray-200">
-			</div>
+					<div class="flex flex-col gap-2 py-2 w-full max-w-28">
+						<div class="w-10">CEP</div>
+						<input type="text" v-maska="'#####-###'" v-model="cep"
+							class="border-2 border-gray-400 rounded-lg p-1 w-full" />
+					</div>
 
-			<div class="flex gap-2 py-4">
-				<div>observações ou comentários</div>
-				<input type="text" class="bg-gray-200">
-			</div> -->
+					<div class="flex flex-col gap-2 py-2 max-w-16">
+						<div class="w-10">UF</div>
+						<input type="text" v-model="uf" class="border-2 border-gray-400 rounded-lg p-1 w-full" />
+					</div>
+
+					<div class="flex flex-col gap-2 py-2 w-full">
+						<div class="w-10">Cidade</div>
+						<input type="text" v-model="cidade" class="border-2 border-gray-400 rounded-lg p-1 w-full" />
+					</div>
+
+					<div class="flex flex-col gap-2 py-2 max-w-40">
+						<div class="w-10">Número</div>
+						<input type="text" class="border-2 border-gray-400 rounded-lg p-1 w-full" />
+					</div>
+				</div>
+
+				<div class="flex flex-col md:flex-row md:gap-6 w-full">
+					<div class="flex flex-col gap-2 py-2 w-full">
+						<div class="w-10">Rua</div>
+						<input type="text" v-model="rua" class="border-2 border-gray-400 rounded-lg p-1 w-full" />
+					</div>
+					<div class="flex flex-col gap-2 py-2 w-full">
+						<div class="w-10">Bairro</div>
+						<input type="text" v-model="bairro" class="border-2 border-gray-400 rounded-lg p-1 w-full" />
+					</div>
+				</div>
+			</div>
 		</main>
 	</div>
 </template>
