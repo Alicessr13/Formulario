8db929883682f2834bf53e3308b49a1e36a37d2b
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2025-04-25 23:01:42 -0300

    feat: mascaramento cpf

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { vMaska } from "maska/vue"
 </script>
 
 <template>
 	<div class="w-screen h-screen flex flex-col items-center">
-		<header class="flex items-center justify-center p-6">
-			<div class="text-3xl">Formulário</div>
+		<header class="flex items-center justify-center p-4">
+			<div class="text-2xl">Formulário</div>
 		</header>
 		<main
-			class="flex flex-col justify-center w-5/6 lg:w-3/4 xl:w-3/5 text-xl bg-white border-2 border-gray-400 p-4 rounded-lg">
-			<div class="flex flex-col md:flex-row gap-2 py-4 md:items-center">
+			class="flex flex-col justify-center w-5/6 lg:w-3/4 xl:w-3/5 text-base bg-white border-2 border-gray-400 p-4 rounded-lg">
+			<div class="flex flex-col md:flex-row gap-2 py-2 md:items-center">
 				<div class="w-full md:w-50">Nome completo:</div>
 				<input type="text" class="border-2 border-gray-400 rounded-lg p-1 w-full" />
 			</div>
 
 			<div class="flex justify-between flex-col lg:flex-row lg:gap-6">
-				<div class="flex flex-col md:flex-row gap-2 py-4 md:items-center">
+				<div class="flex flex-col md:flex-row gap-2 py-2 md:items-center">
 					<div class="w-50">Data de nascimento:</div>
 					<input type="date" class="border-2 border-gray-400 rounded-lg p-1" />
 				</div>
 
-				<div class="flex flex-col md:flex-row gap-2 py-4 md:items-center w-full">
+				<div class="flex flex-col md:flex-row gap-2 py-2 md:items-center w-full">
 					<div class="w-50">CPF ou CNPJ</div>
-					<input type="text" class="border-2 border-gray-400 rounded-lg p-1 w-full" />
+					<input v-maska="'###.###.###-##'" type="text" class="border-2 border-gray-400 rounded-lg p-1 w-full" />
 				</div>
 			</div>
 
