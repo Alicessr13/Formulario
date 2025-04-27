bf0a3dc0e55b444ce1b7ce8d35b1c9e542dfad65
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2025-04-26 21:11:01 -0300

    feat: gerar pdf

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
-import { vMaska } from "maska/vue"
-import { ref, watch } from 'vue'
+import { vMaska } from "maska/vue";
+import { ref, watch } from 'vue';
 
 const cep = ref("");
 const uf = ref("");
 const cidade = ref("");
 const rua = ref("");
 const bairro = ref("");
+const pdfContent = ref<HTMLElement | null>(null);
+
+const imprimir = () => {
+	window.print();
+}
+
 
 watch([cep], async ([novoCep], []) => {
 	if (novoCep.length !== 9) {
@@ -40,8 +46,8 @@ watch([cep], async ([novoCep], []) => {
 		<header class="flex items-center justify-center p-4">
 			<div class="text-2xl">Formulário</div>
 		</header>
-		<main
-			class="flex flex-col justify-center w-5/6 lg:w-3/4 xl:w-3/5 text-base bg-white border-2 border-gray-400 p-4 rounded-lg">
+		<main ref="pdfContent"
+			class="flex flex-col justify-center w-5/6 lg:w-3/4 xl:w-3/5 text-sm bg-white border-2 border-gray-400 p-4 rounded-lg">
 
 			<div class="flex flex-col justify-between md:flex-row md:gap-6">
 				<div class="flex flex-col gap-2 pb-2 w-full">
@@ -111,7 +117,27 @@ watch([cep], async ([novoCep], []) => {
 				</div>
 			</div>
 		</main>
+		<footer class="flex flex-row items-center gap-8">
+			<button class="mt-4 px-4 py-2 bg-black text-white rounded-lg no-print flex items-center gap-2"
+				@click="imprimir">
+				<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="white">
+					<path fill-rule="evenodd" clip-rule="evenodd"
+						d="M13.85 4.44l-3.28-3.3-.35-.14H2.5l-.5.5V7h1V2h6v3.5l.5.5H13v1h1V4.8l-.15-.36zM10 5V2l3 3h-3zM2.5 8l-.5.5v6l.5.5h11l.5-.5v-6l-.5-.5h-11zM13 13v1H3V9h10v4zm-8-1h-.32v1H4v-3h1.06c.75 0 1.13.36 1.13 1a.94.94 0 0 1-.32.72A1.33 1.33 0 0 1 5 12zm-.06-1.45h-.26v.93h.26c.36 0 .54-.16.54-.47 0-.31-.18-.46-.54-.46zM9 12.58a1.48 1.48 0 0 0 .44-1.12c0-1-.53-1.46-1.6-1.46H6.78v3h1.06A1.6 1.6 0 0 0 9 12.58zm-1.55-.13v-1.9h.33a.94.94 0 0 1 .7.25.91.91 0 0 1 .25.67 1 1 0 0 1-.25.72.94.94 0 0 1-.69.26h-.34zm4.45-.61h-.97V13h-.68v-3h1.74v.55h-1.06v.74h.97v.55z" />
+				</svg>
+				Gerar PDF</button>
+
+		</footer>
 	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@media print {
+	.no-print {
+		display: none;
+	}
+
+	body {
+		margin: 1cm;
+	}
+}
+</style>
